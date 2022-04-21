8db7c111e1617763392a5b3ba3c5bd8617480c7b
aziz-sbl <73797346+aziz-sbl@users.noreply.github.com>
2022-04-22 00:47:33 +0100

    Add files via upload

--- contact.php
+++ contact.php
@@ -5,13 +5,14 @@ $message=$_POST['message'];
 $c=mysqli_connect('localhost','root','','tuto');
 if(!$c){die(mysqli_connect_error());}
 else{
-   echo "connection effectué" ;
+   echo 'connection effectué' ;
 }
-$req=mysqli_query($c,"insert into user(name,age,phone,sexe,type_p,type_d,mail,mdp,image) values('$name','$age','$phone','$gender','$type_p','$type_d','$mail','$mdp','$image') ");
+$req=mysqli_query($c,"insert into messages(name,mail,message) values('$name','$email','$message')");
 			if ($req) {
-				move_uploaded_file($image_tmp_name, $image_folder);
-				header('location:my_account.php'); 	
-			} else {
+			echo'"<script>alert("message sent")</script>"' ; 	
+			} 
+            else {
 				echo "Something Wrong Went";
 			}
-		}
+		
+?>
